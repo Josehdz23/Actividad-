4a386f70da6faeaf6914b17d5433ab80012edfb7
Josehdz23 <jose.jahr.2016@gmail.com>
2025-08-08 21:03:16 -0600

    Inicio de programa, menú y main

--- Actividad 14.py
+++ Actividad 14.py
@@ -1,1 +1,23 @@
-print("Hola Mundo")
+def menu():
+    print("\n= = = = MENÚ DE OPCIONES = = = =\n1. Agregar participante\n2. Mostrar participantes ordenados por nombre\n3. Mostrar participantes ordenados por edad\n4. Salir")
+
+def main():
+    while True:
+        try:
+            menu()
+            opcion = int(input("Seleccione una opción: "))
+            match opcion:
+                case 1:
+                    print("1")
+                case 2:
+                    print("2")
+                case 3:
+                    print("3")
+                case 4:
+                    print("\nS A L I E N D O . . .")
+                    break
+                case _:
+                    print("Opción inexistente, reintente")
+        except Exception as ex:
+            print(f"Ha ocurrido un error: {ex}")
+main()
